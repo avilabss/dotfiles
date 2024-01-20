9e76fc7a489abfa1242709013d37791ad60f52f7
git-avilabs <mail.avinashsah@gmail.com>
2024-01-20 15:25:37 +0530

    nvchad custom

---
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,20 @@
+local config = require("plugins.configs.lspconfig")
+
+local on_attach = config.on_attach
+local capabilities = config.capabilities
+
+local lspconfig = require("lspconfig")
+
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"python"},
+})
+
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"rust"},
+  root_dir = lspconfig.util.root_pattern("Cargo.toml"),
+})
+
